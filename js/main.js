9c6b7c70915c89bfb397959c9d33a1f91ab18843
Ahmad Alimuddin <ahmadalimuddin@outlook.com>
2016-10-28 14:11:24 +0700

    waroengKita

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$('#sl2').slider();
+	var RGBChange = function() {
+	  $('#RGB').css('background', 'rgb('+r.getValue()+','+g.getValue()+','+b.getValue()+')')
+	};	
+
+
+/*
+Author : 
+	Team Name 	: WaroengKita
+	Team Leader : Ahmad Alimuddin
+	Team Member : 
+		1. M. Singgih Prasetyo
+		2. Raden Isum
+		3. Ilafi Firsta
+		4. Maudy Nuryanti
+
+Company / Institution 
+	Alan MediaTech Indonesia
+
+Company Site : 
+	http://alan-inc.com
+
+University 	: 
+	Paramadina University
+
+Major 		: 
+	Informatics
+
+Phone 		: 081918305647; 
+
+*/
